73f9be783ae83563dc812ed9850412be981fa1e0
Aluno Shift <alunoshift@fiap.com.br>
2016-12-02 18:14:45 -0200

    First commit

---
+++ Fiap.Exemplo04.Web.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fiap.Exemplo04.Web.MVC.Startup))]
+namespace Fiap.Exemplo04.Web.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
